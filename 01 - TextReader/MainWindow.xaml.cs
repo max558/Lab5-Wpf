966d36f5d066b871dad578e45792c5b18ac3bf20
mivvlk <mivvlk@yandex.ru>
2021-12-22 17:13:22 +0300

    Добавьте файлы проекта.

---
+++ 01 - TextReader/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+/*
+ * Доработать проект текстового редактора из задания 3, добавив главное меню. 
+ * Главное меню должно содержать пункт «Файл» с подпунктами «Открыть», «Сохранить», «Закрыть». 
+ * Добавить обработчики выбора пунктов меню.
+ */
+
+namespace TextReader
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged_Font(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (texBox != null)
+            {
+                texBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_Size(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (texBox != null)
+            {
+                texBox.FontSize = Convert.ToDouble(fontSize);
+            }
+        }
+
+        private void ButtonClick_Boid(object sender, RoutedEventArgs e)
+        {
+            if (texBox == null)
+            {
+                return;
+            }
+            if (texBox.FontWeight > FontWeight.FromOpenTypeWeight(400))
+            {
+                texBox.FontWeight = FontWeight.FromOpenTypeWeight(400);
+            }
+            else
+            {
+                texBox.FontWeight = FontWeight.FromOpenTypeWeight(700);
+            }
+        }
+
+        private void ButtonClick_Italic(object sender, RoutedEventArgs e)
+        {
+            if (texBox == null)
+            {
+                return;
+            }
+            if (texBox.FontStyle == FontStyles.Italic)
+            {
+                texBox.FontStyle = FontStyles.Normal;
+            }
+            else
+            {
+                texBox.FontStyle = FontStyles.Italic;
+            }
+        }
+
+        private void ButtonClick_UnderLine(object sender, RoutedEventArgs e)
+        {
+            if(texBox == null)
+            {
+                return;
+            }
+
+            if (texBox.TextDecorations == TextDecorations.Underline)
+            {
+                texBox.TextDecorations = null;
+            }
+            else
+            {
+                texBox.TextDecorations = TextDecorations.Underline;
+            }
+        }
+
+        private void RadioButton_Checked_Black(object sender, RoutedEventArgs e)
+        {
+            if (texBox == null)
+            {
+                return;
+            }
+            texBox.Foreground = Brushes.Black;
+        }
+
+        private void RadioButton_Checked_Red(object sender, RoutedEventArgs e)
+        {
+            if (texBox == null)
+            {
+                return;
+            }
+
+            texBox.Foreground = Brushes.Red;
+        }
+
+        private void MenuItem_Click_Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if(openFileDialog.ShowDialog()==true)
+            {
+                texBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void MenuItem_Click_Save(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, texBox.Text);
+            }
+        }
+
+        private void MenuItem_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ 02 - GraphicReader/MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Ink;
+
+namespace _02___GraphicReader
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /*
+     * На основе контейнера компоновки InkCanvas разработать графический редактор с функциями рисования мышью и стирания мышью. 
+     * В приложении должно присутствовать главное меню с пунктами «Открыть», «Сохранить» и «Выход», а также панель ToolBar с кнопками для выбора режима. 
+     * Дизайн приложения и, по желанию, дополнительную функциональность разработать самостоятельно.
+     */
+
+    public class PropertyCanvas
+    {
+        InkCanvas inkCanvas;
+        public PropertyCanvas(InkCanvas inc)
+        {
+            inkCanvas = inc;
+        }
+        public void ColorDraw(int iDcolor)
+        {
+            switch (iDcolor)
+            {
+                case 1:
+                    inkCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+                    break;
+                case 2:
+                    inkCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+                    break;
+                case 3:
+                    inkCanvas.DefaultDrawingAttributes.Color = Colors.Yellow;
+                    break;
+
+                default:
+                    inkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+                    break;
+            }
+        }
+        public void SizePen(double heigtPen, double widthPen)
+        {
+            inkCanvas.DefaultDrawingAttributes.Width = widthPen;
+            inkCanvas.DefaultDrawingAttributes.Height = heigtPen;
+        }
+        public void AllClear()
+        {
+            inkCanvas.Strokes.Clear();
+        }
+        public void Eraser()
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+        public void Pincil()
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+    }
+
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click_OpenGraphic(object sender, RoutedEventArgs e)
+        {
+            Image imageControl;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Графические файлы (*.bmp, *.png, *.jpg)|*.bmp;*.png;*.jpg|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() != true)
+            {
+                return;
+            }
+
+
+            /*
+             * Здесь не много не доразобрался может быть особенность Canvas:
+             * Картинка открывается, но как подложка, т.е. рисовать на ней можно удалять (только только что нарисованное), 
+             * НО вот ее (подгруженную картинку) редактировать не получется.
+             */
+
+            try
+            {
+                //Получение картинки
+                BitmapImage image = new BitmapImage(new Uri(openFileDialog.FileName));
+                imageControl = new Image();
+                imageControl.Source = image;                
+            }
+            catch //(Exception ex)
+            {
+                MessageBox.Show("Ошибка чтения картинки.");
+                return;
+            }
+            //Загрузка изображения в Canvas
+            if (imageControl!=null)
+            {
+               graphicRead.Children.Add(imageControl);
+                
+            }
+        }
+
+        private void MenuItem_Click_SaveGraphic(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Графические файлы (*.bmp, *.png, *.jpg)|*.bmp;*.png;*.jpg|Все файлы (*.*)|*.*";
+            saveFileDialog.Title = "Сохранить как...";
+            if (saveFileDialog.ShowDialog()!=true)
+            {
+                return;
+            }
+            var rtb = new RenderTargetBitmap((int)graphicRead.ActualWidth, (int)graphicRead.ActualHeight, 96d, 96d, PixelFormats.Pbgra32);
+            rtb.Render(graphicRead);
+            
+            PngBitmapEncoder BufferSave = new PngBitmapEncoder();
+            BufferSave.Frames.Add(BitmapFrame.Create(rtb));
+            BufferSave.Save(File.OpenWrite(saveFileDialog.FileName));
+        }
+
+        private void MenuItem_Click_ExitGraphic(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ComboBox_SelectionChanged_Color(object sender, SelectionChangedEventArgs e)
+        {
+            int idSelColor = (sender as ComboBox).SelectedIndex;
+            if (graphicRead == null)
+            {
+                return;
+            }
+            PropertyCanvas propertyCanvas = new PropertyCanvas(graphicRead);
+            propertyCanvas.ColorDraw(idSelColor);
+        }
+
+        private void Button_Click_Small(object sender, RoutedEventArgs e)
+        {
+            PropertyCanvas propertyCanvas = new PropertyCanvas(graphicRead);
+            propertyCanvas.SizePen(2,2);
+        }
+
+        private void Button_Click_Medium(object sender, RoutedEventArgs e)
+        {
+            PropertyCanvas propertyCanvas = new PropertyCanvas(graphicRead);
+            propertyCanvas.SizePen(7, 7);
+        }
+
+        private void Button_Click_Big(object sender, RoutedEventArgs e)
+        {
+            PropertyCanvas propertyCanvas = new PropertyCanvas(graphicRead);
+            propertyCanvas.SizePen(15, 15);
+        }
+
+        private void Button_Click_AllClear(object sender, RoutedEventArgs e)
+        {
+            PropertyCanvas propertyCanvas = new PropertyCanvas(graphicRead);
+            propertyCanvas.AllClear();
+        }
+
+        private void Button_Click_Eraser(object sender, RoutedEventArgs e)
+        {
+            PropertyCanvas propertyCanvas = new PropertyCanvas(graphicRead);
+            propertyCanvas.Eraser();
+        }
+
+        private void Button_Click_Inc(object sender, RoutedEventArgs e)
+        {
+            PropertyCanvas propertyCanvas = new PropertyCanvas(graphicRead);
+            propertyCanvas.Pincil();
+        }
+    }
+}
